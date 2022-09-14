3ec0e4db6690cdab2d3dd2997cc93b04d0584b40
Dave <112990789+Dave-Gamez@users.noreply.github.com>
2022-09-14 10:59:49 -0500

    Update Setup_script.sh
    Changed the website in Google Drive to be a bit cleaner.

--- Setup_script.sh
+++ Setup_script.sh
@@ -4,7 +4,7 @@ sudo yum install -y httpd
 sudo yum install -y wget
 sudo yum install -y unzip
 cd /var/www/html
-sudo wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1-SrPNSJ09enApgfp0yRrEqgw8PmJpCg-' -O /var/www/html/index.zip
-sudo wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1938nX2DrMDKqZdTSp4BcrCTM_3Ck6j7I' -O /var/www/html/index.html
+sudo wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1HRrO4pFRdVVrw5wEpgPdBw70-H_ty0pe' -O /var/www/html/index.zip
+sudo wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1EKtIBUrCdhP5YTeTvC_aD4xHEHVTNFbJ' -O /var/www/html/index.html
 sudo unzip -o index.zip
 sudo service httpd restart

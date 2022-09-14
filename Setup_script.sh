6aff231a681be7ef5ba95c72797c4a22673fad2c
Dave <112990789+Dave-Gamez@users.noreply.github.com>
2022-09-14 11:50:03 -0500

    Update Setup_script.sh
    Added copy of html file for links to work correctly.  They point to index.htm

--- Setup_script.sh
+++ Setup_script.sh
@@ -7,4 +7,5 @@ cd /var/www/html
 sudo wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1HRrO4pFRdVVrw5wEpgPdBw70-H_ty0pe' -O /var/www/html/index.zip
 sudo wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1EKtIBUrCdhP5YTeTvC_aD4xHEHVTNFbJ' -O /var/www/html/index.html
 sudo unzip -o index.zip
+cp /var/www/html/index.html /var/www/html/index.htm
 sudo service httpd restart
